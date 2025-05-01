3f94dfe69fe5d4688dfa0b047d403a42c7f6b098
Medigone <admin@medigo.one>
2025-05-02 01:03:19 +0200

    refactor: remove Vite logo and update script paths
    The Vite logo was removed from the App component as it was no longer necessary. Additionally, the script paths in the HTML files were updated to reflect the latest build assets.

--- mp/src/App.tsx
+++ mp/src/App.tsx
@@ -10,9 +10,7 @@ function App() {
 	  <FrappeProvider>
 		<div>
 	  <div>
-		<a href="https://vitejs.dev" target="_blank">
-		  <img src="/vite.svg" className="logo" alt="Vite logo" />
-		</a>
+
 		<a href="https://reactjs.org" target="_blank">
 		  <img src={reactLogo} className="logo react" alt="React logo" />
 		</a>
