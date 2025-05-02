99cea1534bf8c341a68b4a07e779e9e115384c83
Medigone <admin@medigo.one>
2025-05-02 15:43:03 +0200

    feat(mp): integrate TailwindCSS and Radix UI for styling
    add TailwindCSS configuration and integrate Radix UI for improved styling and theming. update dependencies to include TailwindCSS, Radix UI, and related packages. modify App.tsx to use Radix UI's Theme component and TailwindCSS for styling. update index.html to include necessary scripts for bootstrapping the application

--- mp/src/App.tsx
+++ mp/src/App.tsx
@@ -2,12 +2,33 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { FrappeProvider } from 'frappe-react-sdk'
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
+
 function App() {
   const [count, setCount] = useState(0)
 
+	const getSiteName = () => {
+		// @ts-ignore
+		if (window.frappe?.boot?.versions?.frappe && (window.frappe.boot.versions.frappe.startsWith('15') || window.frappe.boot.versions.frappe.startsWith('16'))) {
+			// @ts-ignore
+			return window.frappe?.boot?.sitename ?? import.meta.env.VITE_SITE_NAME
+		}
+		return import.meta.env.VITE_SITE_NAME
+
+	}
+
   return (
 	<div className="App">
-	  <FrappeProvider>
+		<Theme 
+			appearance='dark'
+			accentColor='gray'
+			panelBackground='translucent'
+		
+		>
+	  <FrappeProvider
+	  		siteName={getSiteName()}
+	  >
 		<div>
 	  <div>
 
@@ -15,7 +36,7 @@ function App() {
 		  <img src={reactLogo} className="logo react" alt="React logo" />
 		</a>
 	  </div>
-	  <h1>Vite + React + Frappe</h1>
+	  <h1>MP + React + Frappe</h1>
 	  <div className="card">
 		<button onClick={() => setCount((count) => count + 1)}>
 		  count is {count}
@@ -25,6 +46,7 @@ function App() {
 	  
 	  </div>
 	  </FrappeProvider>
+	  </Theme>	
 	</div>
   )
 }
